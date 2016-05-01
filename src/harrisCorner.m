1b5fc49d4af03eaca7cc554176edd5228b711e7d
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2016-05-02 00:02:42 +0800

    clean code

--- src/harrisCorner.m
+++ src/harrisCorner.m
@@ -1,4 +1,4 @@
-function [cornerMap,Gx,Gy,R] = harrisCorner(imgIn,sigma1,sigma2,k)
+function [cornerMap,R] = harrisCorner(imgIn,sigma1,sigma2,k)
 	if(~exist('sigma1','var'))
 		sigma1 = 1.5;
 	end
@@ -14,15 +14,15 @@ function [cornerMap,Gx,Gy,R] = harrisCorner(imgIn,sigma1,sigma2,k)
 		grayImg = imgIn;
 	end
 	g_denoise = fspecial('gaussian', [3 3], sigma1);
-	grayImg = imfilter(grayImg,g_denoise,'same');
+	grayImg = imfilter(grayImg,g_denoise,'same','conv');
 	g = fspecial('gaussian',[8 8],sigma2);
 	[Gx, Gy] = imgradientxy(grayImg,'prewitt');
 	Gx2 = Gx .* Gx;
 	Gy2 = Gy .* Gy;
 	Gxy = Gx .* Gy;
-	Sx2 = imfilter(Gx2,g,'same');
-	Sy2 = imfilter(Gy2,g,'same');
-	Sxy = imfilter(Gxy,g,'same');
+	Sx2 = imfilter(Gx2,g,'same','conv');
+	Sy2 = imfilter(Gy2,g,'same','conv');
+	Sxy = imfilter(Gxy,g,'same','conv');
 	cornerMap = zeros(size(grayImg));
 	detH = Sx2.*Sy2 - Sxy.*Sxy;
 	traceH = Sx2 + Sy2;
--- src/testHarrisCorner.m
+++ src/testHarrisCorner.m
@@ -2,7 +2,10 @@ close all
 clear
 clc
 I = checkerboard;
-% I = imread('../data/csie/IMG_8709.JPG');
-% I = im2double(I);
-[cornerMap,Gx,Gy,R]= harrisCorner(I);
+[cornerMap,R]= harrisCorner(I);
+imshow(cornerMap);
+I = imread('../data/csie/IMG_8709.JPG');
+I = im2double(I);
+[cornerMap,R]= harrisCorner(I);
+figure;
 imshow(cornerMap);
