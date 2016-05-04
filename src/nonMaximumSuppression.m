d43f91ec2e1344cc7d152485efcdd3e0bd607d86
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2016-05-05 02:35:27 +0800

    minor

--- src/nonMaximumSuppression.m
+++ src/nonMaximumSuppression.m
@@ -1,9 +1,24 @@
-function [outMap] = nonMaximumSuppression(inMap,R,num)
-	%% under implement
-	% index = find(inMap==1);
-	% r = R(index);
-	% [sorted_r order] = sort(r);
-	% sorted_index = index(order);
+function [featureMapOut] = nonMaximumSuppression(featureMapIn,R,num)
+	%% still under implement
+	index = find(featureMapIn==1);
+	r = R(index);
+	[sorted_r order] = sort(r);
+	sorted_index = index(order);
+	featureMapOut = zeros(size(featureMapIn));
+	counter = 0;
+	[sorted_row,sorted_col] = ind2sub(size(featureMapIn),sorted_index);
+	dist_max = size(featureMapIn,1)^2 + size(featureMapIn,2)^2 );
+	while(counter<num)
+		% dist = dist_max;
+		% while(dist>0)
+		% 	for i = 1:size(sorted_index)
+		% 		if()
+		% 			dist
+		% 		end
+		% 	end
+		% 	dist = dist - 1;
+		% end
+	end
 	% sorted_sub = ind2sub(sorted_index);
 	% outMap = zeros(size(inMap));
 	% size_count = 0;
--- src/testHarrisCorner.m
+++ src/testHarrisCorner.m
@@ -1,12 +1,12 @@
 close all
 clear
 clc
-% I = checkerboard;
-% I = I*255;
-% [cornerMap,R]= harrisCorner(I);
-% imshow(cornerMap);
-% figure;
-% mesh(R);
+I = checkerboard;
+I = I*255;
+[cornerMap,R]= harrisCorner(I);
+imshow(cornerMap);
+figure;
+mesh(R);
 I = imread('../data/csie/IMG_8709.JPG');
 I = double(I);
 [cornerMap,R]= harrisCorner(I);
