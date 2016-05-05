b37bcc5762b6c154b00862ee24087cedc48126cf
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2016-05-06 02:51:18 +0800

    save before partner edit

--- src/nonMaximumSuppression.m
+++ src/nonMaximumSuppression.m
@@ -9,15 +9,47 @@ function [featureMapOut] = nonMaximumSuppression(featureMapIn,R,num)
 	[sorted_row,sorted_col] = ind2sub(size(featureMapIn),sorted_index);
 	dist_max = size(featureMapIn,1)^2 + size(featureMapIn,2)^2 );
 	while(counter<num)
-		% dist = dist_max;
-		% while(dist>0)
-		% 	for i = 1:size(sorted_index)
-		% 		if()
-		% 			dist
-		% 		end
-		% 	end
-		% 	dist = dist - 1;
-		% end
+		if(counter)==0
+			extracted_index = sorted_index(1);
+			sorted_row = sorted_row(2:end);
+			sorted_col = sorted_col(2:end);
+			sorted_index = sorted_index(2:end);
+			sorted_r = sorted_r(2:end);
+			counter = 1;
+		else
+			dist = dist_max;
+			while(dist>0)
+				for i = 1:size(sorted_index)
+					before_num = size(extracted_index,1);
+					% canBeChosen = true;
+					[row1,col1] = ind2sub(size(featureMapIn),sorted_index(i));
+					for j = 1:size(extracted_index)
+						[row2,col2] =  ind2sub(size(featureMapIn),extracted_index(j));
+						if( (row1-row2)^2 + (col1-col2)^2 < dist)
+							if(j==size(extracted_index))
+								extracted_index = [ extracted_index; sorted_index(i)];
+								counter = counter + 1;
+							else
+								continue;
+							end
+						else
+							break;
+						end
+					end
+					if(size(extracted_index,1)~=before_num)
+						if(counter==num)
+							dist = dist_max + 1;
+						else
+							dist = dist_max;
+						end
+						break;
+					end
+					if(dist == dist_max + 1)
+						break;
+					end
+				end
+			end
+		end
 	end
 	% sorted_sub = ind2sub(sorted_index);
 	% outMap = zeros(size(inMap));
